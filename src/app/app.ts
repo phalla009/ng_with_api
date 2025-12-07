e8f119e76c28fadd89dfc9e96b22f4aa7a93ac5e
phalla009 <hphalla3@gmail.com>
2025-12-07 23:10:15 +0700

    full

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Home } from './home/home';
+import { Footer } from "./footer/footer";
+import { Navbar } from "./navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, Home, Footer, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('ng_with_api');
+
 }
