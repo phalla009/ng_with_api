4d9f75032855dd8fa6b2f3b6d6e2751eeff47f4a
phalla009 <hphalla3@gmail.com>
2025-12-07 23:48:49 +0700

    edit css

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { Home } from './home/home';
 import { Footer } from "./footer/footer";
 import { Navbar } from "./navbar/navbar";
+import { Benner } from "./benner/benner";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, Home, Footer, Navbar],
+  imports: [RouterOutlet, FormsModule, Home, Footer, Navbar, Benner],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/benner/benner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-benner',
+  imports: [],
+  templateUrl: './benner.html',
+  styleUrl: './benner.css',
+})
+export class Benner {
+
+}
